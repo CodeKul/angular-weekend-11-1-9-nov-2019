76d717c2db279c350cb8d37a43f9fc0ef132f428
aniruddha <aniruddha.kudalkar@gmail.com>
2019-11-17 12:05:13 +0530

    js : basics and es6

---
+++ basics/vars-fn.js
@@ -0,0 +1,13 @@
+var num1 = 10
+var num2 = 20
+console.log(num1 + num2)
+
+// console.log(console)
+
+function makeCalculation(n1, n2) {
+    console.log(n1 + n2)
+
+    return n1 + n2
+}
+
+makeCalculation(89, 75)
