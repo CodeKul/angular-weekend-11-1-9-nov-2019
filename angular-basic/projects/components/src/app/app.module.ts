785fd8b96d0f1d8c2f20536a134a9d31da321add
aniruddha <aniruddha.kudalkar@gmail.com>
2019-11-30 13:05:14 +0530

    components : cli and understanding of components

--- angular-basic/projects/components/src/app/app.module.ts
+++ angular-basic/projects/components/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DefinationComponent } from './defination/defination.component';
+import { CodeBtnComponent } from './code-btn/code-btn.component';
+import { RoundBtnComponent } from './code-btn/round-btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefinationComponent,
+    CodeBtnComponent,
+    RoundBtnComponent
   ],
   imports: [
     BrowserModule
