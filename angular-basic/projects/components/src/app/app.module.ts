ed5ce66be88ed1cd54d71625bfdd79e793f66571
aniruddha <aniruddha.kudalkar@gmail.com>
2019-11-30 11:44:22 +0530

    components : project created

--- angular-basic/projects/components/src/app/app.module.ts
+++ angular-basic/projects/components/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DefCompComponent } from './def-comp/def-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DefCompComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
