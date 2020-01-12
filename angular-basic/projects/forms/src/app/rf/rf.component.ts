5f4d6ed6d5720a946788a095b8ceeaaead04d2b7
aniruddha <aniruddha.kudalkar@gmail.com>
2020-01-12 11:35:27 +0530

    forms : reactive forms

--- angular-basic/projects/forms/src/app/rf/rf.component.ts
+++ angular-basic/projects/forms/src/app/rf/rf.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rf',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RfComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    const pa = Validators.compose([
+      Validators.required,
+      Validators.email
+    ])
+    
+    this.form = this.fb.group({
+      email: this.fb.control('', pa),
+      password: this.fb.control('', Validators.required),
+      address: this.fb.control('', Validators.required),
+      address2: this.fb.control(''),
+      city: this.fb.control('', Validators.required),
+      state: this.fb.control('', Validators.required),
+      zip: this.fb.control(''),
+      keepMe: this.fb.control(false)
+    })
   }
 
+  frmSub() {
+    console.log(`Form Submitted`)
+    console.log(this.form)
+  }
+
+  formControlValidationStatus(name: string) {
+    return this.form.get(name).valid
+  }
 }
